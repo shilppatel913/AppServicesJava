850eeaaa3b3823857c80de42dae375a906d08502
shilppatel913 <shilppatel77@gmail.com>
2023-01-23 23:48:44 +0530

    This will be deployed on app services

---
+++ src/main/java/com/aster/app/AppservicesApplication.java
@@ -0,0 +1,14 @@
+package com.aster.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppservicesApplication.class, args);
+		System.out.println("Application has started running");
+	}
+
+}
